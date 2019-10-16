55074fdafabbdcbfda6354098e3bbdf59aea76c2
Ofer Ben-Yacov <ofer.benyacov@toganetworks.com>
2019-10-16 10:05:08 +0300

    multiple skills give single step priority

--- src/main/java/com/huawei/vca/nlg/ResponseGenerator.java
+++ src/main/java/com/huawei/vca/nlg/ResponseGenerator.java
@@ -1,12 +1,16 @@
 package com.huawei.vca.nlg;
 
 import com.huawei.vca.message.BotUtterEvent;
+import com.huawei.vca.repository.entity.BotUtterEntity;
+import com.huawei.vca.repository.entity.MenuItemEntity;
 import com.huawei.vca.repository.entity.SlotEntity;
 
 public interface ResponseGenerator {
 
     BotUtterEvent generateResponse(String actionId);
 
-    BotUtterEvent getQueryForSlot(SlotEntity slotEntity);
+    BotUtterEntity generateResponse(MenuItemEntity menuItemEntity);
+
+    BotUtterEvent generateQueryResponseForSlot(SlotEntity slotEntity);
 
 }
